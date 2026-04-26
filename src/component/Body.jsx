03588be2fdc1fdb013da540cd0ffd645f532cb1e
byungkwon <ansqud14@gmail.com>
2026-04-26 20:23:41 +0900

    first commit

---
+++ src/component/Body.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Body(){
+    return(
+        <div style={{ backgroundColor:"red",color:"blue"}}>
+            <h1>body</h1>
+        </div>
+    );
+}
+export default Body;
